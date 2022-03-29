b3c97176366629f9203b2f72ba6908ddc906a780
Юрий Ивайкин <skierorama@gmail.com>
2022-03-30 01:46:17 +0300

    Adding Category, Genre, Title into nodels and admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, Title
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'year', 'rating', 'description', 'genre', 'category'
+    )
+    search_fields = ('name',)
+    list_filter = ('year', 'rating', 'genre', 'category')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
