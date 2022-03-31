efb7d332cdcb030b02999fe05f9ffd8fb2fd65a8
Ясин Ализаде <yasin.alizade@yandex.ru>
2022-03-31 14:49:49 +0300

    Add perform create to CommentViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -26,3 +26,14 @@ class CommentViewSet(viewsets.ModelViewSet):
         title = get_object_or_404(Title, pk=title_id)
         review = get_object_or_404(Review, pk=review_id)
         return queryset.filter(title=title, review=review)
+
+    def perform_create(self, serializer, *args, **kwargs):
+        title_id = self.kwargs.get("title_id")
+        review_id = self.kwargs.get("review_id")
+        title = get_object_or_404(Title, pk=title_id)
+        review = get_object_or_404(Review, pk=review_id)
+        return serializer.save(
+            author=self.request.user,
+            title=title,
+            review=review
+        )
